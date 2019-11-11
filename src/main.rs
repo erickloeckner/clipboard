8026e059448e14368c7858a8f40e26dec7f42182
erickloeckner <48753426+erickloeckner@users.noreply.github.com>
2019-11-11 09:27:04 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,53 @@ struct ButtonConf {
     cmd:  String,
 }
 
+const STYLE: &str = "
+#buttons {
+    background-image: -gtk-gradient (linear,
+                                     0 0, 1 0,
+                                     color-stop(0.0, #606060),
+                                     color-stop(0.5, #7f7f7f),
+                                     color-stop(1.0, #606060));
+    padding-left:   4px;
+    padding-right:  4px;
+    padding-top:    2px;
+    padding-bottom: 2px;
+}
+#buttons:hover {
+    background-image: -gtk-gradient (linear,
+                                     0 0, 1 0,
+                                     color-stop(0.0, #801010),
+                                     color-stop(0.5, #9f2020),
+                                     color-stop(1.0, #801010));
+    padding-left:   4px;
+    padding-right:  4px;
+    padding-top:    2px;
+    padding-bottom: 2px;
+}
+#buttons label {
+    color: #f2f0f0;
+    font-weight: bold;
+}";
+
 fn main() {
     let application =
         gtk::Application::new(Some("com.github.gtk-rs.examples.basic"), Default::default())
             .expect("Initialization failed...");
 
     application.connect_activate(|app| {
+        // The CSS "magic" happens here.
+        let provider = gtk::CssProvider::new();
+        provider
+            .load_from_data(STYLE.as_bytes())
+            .expect("Failed to load CSS");
+        // We give the CssProvided to the default screen so the CSS rules we added
+        // can be applied to our window.
+        gtk::StyleContext::add_provider_for_screen(
+            &gdk::Screen::get_default().expect("Error initializing gtk css provider."),
+            &provider,
+            gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+        );
+        
         build_ui(app);
     });
 
@@ -31,7 +72,8 @@ fn main() {
 
 fn build_ui(application: &gtk::Application) {
     let mut cwd = env::current_exe().unwrap();
-    for _i in 0..3 { cwd.pop(); }
+    //~ for _i in 0..3 { cwd.pop(); }
+    cwd.pop();
     cwd.push("clipboard.conf");
     
     let lines = read_lines(&cwd).unwrap_or_else(|err| {
@@ -53,6 +95,7 @@ fn build_ui(application: &gtk::Application) {
 
     for i in buttons {
         let button = gtk::Button::new_with_label(&i.name);
+        gtk::WidgetExt::set_name(&button, "buttons");
         //~ let cmd_clone = i.cmd.clone();
         
         button.connect_clicked(move |_| {
